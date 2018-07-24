fb2e92f765dad142975334b205a0061b94b4191f
Jefferson Lam <jeffersonwlam@gmail.com>
2018-07-24 16:54:26 -0500

    Add press animation; stop keyrepeat

--- scripts/main.js
+++ scripts/main.js
@@ -6,34 +6,44 @@ var clickerCounterDisplay = document.getElementById("clicker-counter");
 var body = document.getElementById("body");
 var loader = document.getElementById('loader');
 
+var down = false;
+
+// Update firebase DB
+function firebaseClicksIncrement(ref) {
+  ref.transaction(function(clicks) {
+    clicks++;
+    return clicks;
+  });
+}
+
 // Updates number of clicks on clicker button
-function updateClickerCount(num) {
+function updateClickerDisplay(num) {
   clickerCounterDisplay.innerText = num;
+}
+
+function clickerAnimation() {
+  clickerButton.classList.remove("press");
+  clickerButton.offsetWidth;
+  clickerButton.classList.add("press");
 }
 
 // Bindings on load.
 window.addEventListener("load",function() {
-    var clickerClicksRef = firebase.database().ref("/clicks");
+  var clickerClicksRef = firebase.database().ref("/clicks");
 
-    // Automatically sign user in anonymously
-    firebase.auth().signInAnonymously().catch(function(error) {
-        // On error
-        console.log(error.code);
-        console.log(error.message);
-      });
-
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        initializeClicker();
-      }
+  // Automatically sign user in anonymously
+  firebase.auth().signInAnonymously().catch(function(error) {
+      // On error
+      console.log(error.code);
+      console.log(error.message);
     });
 
-    // Listen for clicker clicks counts
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
 
-    function initializeClicker() {
       // Hide loader
       clickerClicksRef.once("value").then(function(){
         body.classList.remove('loading');
@@ -41,17 +51,26 @@ window.addEventListener("load",function() {
 
       // Update clicker count display
       clickerClicksRef.on("value", function(snapshot) {
-        updateClickerCount(snapshot.val());
+        updateClickerDisplay(snapshot.val());
+        clickerAnimation();
       });
     }
+  });
 
-    // Listen for clicks and update DB
-    clickerButton.addEventListener("click", function() {
-      clickerClicksRef.transaction(function(clicks) {
-        clicks++;
-        return clicks;
-      });
-    });
-  },
-  false
-);
+  // Listen for clicks and update DB
+  clickerButton.addEventListener("mouseup", function(e) {
+    firebaseClicksIncrement(clickerClicksRef);
+    clickerAnimation();
+  }, false);
+
+  clickerButton.addEventListener("keyup", function() {
+    down = false;
+  }, false);
+
+  clickerButton.addEventListener("keydown", function(e) {
+    if (e.keyCode != 13 || down) return;
+    down = true;
+    firebaseClicksIncrement(clickerClicksRef);
+    clickerAnimation();
+  }, false);
+}, false);
