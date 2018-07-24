9ba5ca4f16813ed07d19e82699f00e9606f02d40
Jefferson Lam <jeffersonwlam@gmail.com>
2018-07-23 21:04:44 -0500

    Add loading spinner

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
-'use strict';
-var clickerButton = document.getElementById('clicker-button');
-var clickerCounterDisplay = document.getElementById('clicker-counter');
+"use strict";
+var clickerButton = document.getElementById("clicker-button");
+var clickerCounterDisplay = document.getElementById("clicker-counter");
+
+// loader elements
+var body = document.getElementById("body");
+var loader = document.getElementById('loader');
 
 // Updates number of clicks on clicker button
 function updateClickerCount(nbStart) {
@@ -8,18 +12,46 @@ function updateClickerCount(nbStart) {
 }
 
 // Bindings on load.
-window.addEventListener('load', function() {
-  // Listen for clicker clicks counts
-  var clickerClicksRef = firebase.database().ref('/clicks');
+window.addEventListener("load",function() {
+    var clickerClicksRef = firebase.database().ref("/clicks");
 
-  clickerClicksRef.on('value', function(snapshot) {
-    updateClickerCount(snapshot.val());
-  });
+    // Automatically sign user in anonymously
+    firebase.auth().signInAnonymously().catch(function(error) {
+        // On error
+        console.log(error.code);
+        console.log(error.message);
+      });
 
-  clickerButton.addEventListener('click', function() {
-    clickerClicksRef.transaction(function(clicks) {
-      clicks++;
-      return clicks;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        initializeClicker();
+      }
     });
-  });
-}, false);
+
+    // Listen for clicker clicks counts
+
+    function initializeClicker() {
+      // Hide loader
+      clickerClicksRef.once("value").then(function(){
+        body.classList.remove('loading');
+      });
+
+      // Update clicker count display
+      clickerClicksRef.on("value", function(snapshot) {
+        updateClickerCount(snapshot.val());
+      });
+    }
+
+    // Listen for clicks and update DB
+    clickerButton.addEventListener("click", function() {
+      clickerClicksRef.transaction(function(clicks) {
+        clicks++;
+        return clicks;
+      });
+    });
+  },
+  false
+);
